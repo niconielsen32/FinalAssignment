96e29ba34f76e36228159ec221b3c73d3f5ba42e
niconielsen32 <n.hoeirup.n@gmail.com>
2020-05-10 18:41:31 +0200

    working buttons

--- 1_Pan_tilt_project/UserInterface/UI.c
+++ 1_Pan_tilt_project/UserInterface/UI.c
@@ -66,13 +66,31 @@ void UI_task(void * pvParameters)
     while(1)
     {
 
-        if(~GPIO_PORTF_DATA_R & 0x11)
-            button = "button1";
-        else if (GPIO_PORTF_DATA_R &= ~(0x02))
-            button = "button2";
+        int dummy;
+           SYSCTL_RCGC2_R = SYSCTL_RCGC2_GPIOF; // 1) activate clock for Port F
+           dummy = SYSCTL_RCGC2_R;           // allow time for clock to start
+           GPIO_PORTF_LOCK_R = 0x4C4F434B;   // 2) unlock GPIO Port F
+           GPIO_PORTF_CR_R = 0x1F;           // allow changes to PF4-0
+           GPIO_PORTF_DIR_R = 0x0E;
+           GPIO_PORTF_DEN_R = 0x1F;
+           GPIO_PORTF_PUR_R = 0x11;
+
+
+               if(!(GPIO_PORTF_DATA_R & 0x10)){ //sw1
+                   GPIO_PORTF_DATA_R = 0x08; //put it on the green LED
+                   button = "button1";
+               } else if(!(GPIO_PORTF_DATA_R & 0x01)){ //sw2
+                   GPIO_PORTF_DATA_R = 0x02; //put it on the RED LED
+                   button = "button2";
+               } else {
+                   GPIO_PORTF_DATA_R = 0x00;
+                   button = "No button";
+               }
+
 
         write_string(button);
         write_character(' ');
+
 //        // write output
 //        // Motor 1
 //        write_string(sMotor1);
@@ -105,7 +123,7 @@ void UI_task(void * pvParameters)
 //        write_cr();
 
 
-        vTaskDelay(pdMS_TO_TICKS(500));
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
 
 

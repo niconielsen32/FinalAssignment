b14e2c5fd2d31327b9f74dc55f7d9d351585e457
Rebecca Hyldgaard <rehyl18@student.sdu.dk>
2020-05-15 11:49:41 +0200

    LCD_is_showing
    Ændret i UI.c i forhold til at COM1 til COM2, så den går til LCD'en og ikke UART

--- 1_Pan_tilt_project/UserInterface/UI.c
+++ 1_Pan_tilt_project/UserInterface/UI.c
@@ -19,16 +19,32 @@
 *****************************************************************************/
 
 /***************************** Include files *******************************/
+#include "FreeRTOS.h"
+#include "glob_def.h"
 #include "emp_type.h"
-#include <stdint.h>
 #include "tm4c123gh6pm.h"
+#include "string.h"
+#include "file.h"
+#include "LCD.h"
+#include "keypad.h"
+#include "UI.h"
+//Scale.h is missing, what is scale? -> Not even nesessary in his program
+//Semphr.h is also missing -> also not nesserary
 #include "ADC.h"
+<<<<<<< Updated upstream
 #include "glob_def.h"
 #include "FreeRTOS.h"
+=======
+#include <stdint.h>
+>>>>>>> Stashed changes
 #include "write.h"
 #include "data.h"
-#include "UI.h"
 #include "buttons.h"
+<<<<<<< Updated upstream
+=======
+#include "tmodel.h"
+>>>>>>> Stashed changes
+
 
 /*****************************    Defines    *******************************/
 
@@ -53,6 +69,7 @@ void UI_task(void * pvParameters)
 
     while(1)
     {
+<<<<<<< Updated upstream
         button = get_button_state;
 
         // test for buttons working
@@ -67,6 +84,58 @@ void UI_task(void * pvParameters)
                    //GPIO_PORTF_DATA_R = 0x00;
                    button = "No button";
                }
+=======
+        INT8U key = 0;
+        gfprintf(COM2, "%c%cValue: %05u", 0x1B, 0x80, adjusted_value);  // the adjusted value is shown on the first line of the display. this is done outside the state machine so it's displayed all the time
+        switch(ui_state)
+        {
+        case 0:
+            gfprintf(COM2, "%c%cScale:         ", 0x1B, 0xA8);          // "Scale:" is printed on the second line of the display
+            key = get_keyboard();                                       // we get a value from the keyboard
+            if( key >= '0' && key <= '9')                               // if it's a number between 0 and 9 we save that value in scale_tmp and go to the next state
+            {
+                scale_tmp = key - '0';                                  // the value from the keyboard is given as an ASCII char, so to convert to the actual value we subtract the ASCII-value for 0
+                ui_state = 1;
+            }
+            break;
+        case 1:
+            gfprintf(COM2, "%c%cOffset:", 0x1B, 0xA8);                  // "Offset:" is printed on the second line of the display
+            key = get_keyboard();                                       // same procedure as in state 0, but we save the value in off1 since we want it as the first digit of the offset value
+            if( key >= '0' && key <= '9')
+            {
+                gfprintf(COM2, "%c%c%c", 0x1B, 0xC9, key);              // the digit is printed on the second line (after "Offset:")
+                off1 = (key - '0')*100;                                 // again we subtract the ASCII for 0. we also multiply by 100 since it's the first of the 3 digits
+                ui_state = 2;
+            }
+            break;
+        case 2:
+            key = get_keyboard();                                       // same procedure for the second digit of the offset value
+            if( key >= '0' && key <= '9')
+            {
+                gfprintf(COM2, "%c%c%c", 0x1B, 0xCA, key);
+                off2 = (key - '0')*10;
+                ui_state = 3;
+            }
+            break;
+        case 3:
+            key = get_keyboard();                                       // same procedure for the third digit of the offset value
+            if( key >= '0' && key <= '9')
+            {
+                gfprintf(COM2, "%c%c%c", 0x1B, 0xCB, key);
+                off3 = (key - '0')*1;
+
+                if( xSemaphoreTake( xMutex, portMAX_DELAY ))            // we want to change the shared variables so we protect them with a mutex
+                {
+                    scale = scale_tmp;
+                    offset = off1 + off2 + off3;
+                    xSemaphoreGive(xMutex);
+                }
+                ui_state = 0;
+            }
+            break;
+        }
+    }
+>>>>>>> Stashed changes
 
 
 //        write_string(button);

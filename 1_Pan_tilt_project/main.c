c6387067ba9a39b9f12f9b22dc0cb8b157c58d08
Rebecca Hyldgaard <rehyl18@student.sdu.dk>
2020-05-21 01:19:41 +0200

    From_here!

--- 1_Pan_tilt_project/main.c
+++ 1_Pan_tilt_project/main.c
@@ -74,7 +74,7 @@ static void setupHardware(void)
 }
 
 TaskHandle_t write_task_handle = NULL;
-//TaskHandle_t report_task_handle = NULL;
+//TaskHandle_t report_task_handle = NULL;  -> needs changing
 TaskHandle_t button_task_handle = NULL;
 TaskHandle_t payment_task_handle = NULL;
 TaskHandle_t keypad_task_handle = NULL;

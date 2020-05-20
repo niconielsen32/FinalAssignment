0ee66f43cda851b7502e220c724265c0cc30302d
Rebecca Hyldgaard <rehyl18@student.sdu.dk>
2020-05-20 23:15:18 +0200

    Update payment.h

--- 1_Pan_tilt_project/payment.h
+++ 1_Pan_tilt_project/payment.h
@@ -28,24 +28,13 @@
 /*****************************   Constants   *******************************/
 
 /*****************************   Functions   *******************************/
-INT16U select_payment_type(INT16U);
-BOOLEAN get_payment_complete();
-void set_payment_complete(BOOLEAN);
-INT16U get_payment_selection();
-INT16U get_payment_type();
-
-
-/*****************************   Constants   *******************************/
-
-/*****************************   Functions   *******************************/
-//INT16U select_payment_type(INT16U);
-BOOLEAN payment_complete();
-INT16U get_payment_selection();
-INT16U get_total_cash();
-INT16U get_pay_type();
-BOOLEAN get_card_valid();
 INT8U last_elemet_queue(QueueHandle_t queue, INT16U queue_size);
+BOOLEAN get_payment_complete();
+void set_payment_complete(BOOLEAN payment_complete);
+INT16U get_payment_type();
+BOOLEAN get_paytype_complete();
 
+void terminate_session();
 
 void payment_task(void* pvParameters);
 /****************************** End Of Module *******************************/
